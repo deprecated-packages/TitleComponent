62fa1ac987445822789ceb6075a8d74c70193eff
Tomas Votruba <info@tomasvotruba.cz>
2014-03-29 13:27:12 +0100

    init

---
+++ readme.md
@@ -0,0 +1,95 @@
+# Zenify/TitleComponent
+
+
+## Requirements
+
+Zenify/User requires PHP 5.4.
+
+- [nette/nette](https://github.com/nette/nette/)
+
+
+## Installation
+
+The best way to install this package is using [Composer](http://getcomposer.org/).
+Add to your `composer.json`:
+
+```js
+"require": {
+	"zenify/title-component": "@dev"
+}
+```
+
+Run `composer update`.
+
+And register the extension in `config.neon`:
+
+```neon
+extensions:
+	- Zenify\Components\TitleComponent\DI\Extension
+```
+
+## Use
+
+Inject to presenter.
+
+```php
+class Presenter ... {
+
+	/** @inject @var Zenify\Components\TitleComponent\UI\IControl */
+	public $titleControl;
+
+
+	public function createComponentTitleControl()
+	{
+		return $this->titleControl->create();
+	}
+
+}
+```
+
+### Add title
+
+1. Via annotation
+
+```php
+class HomepagePresenter ... {
+
+	/**
+	 * @title Contact us
+	 */
+	public function renderContact()
+	{
+	}
+
+}
+```
+
+2. Or with direct access
+
+```php
+class ProductPresenter ... {
+
+	public function renderDetail($id)
+	{
+		$product = ...($id);
+		$this['titleControl']->setTitle('Detail of ' . $product->name);
+	}
+
+}
+```
+
+#### Translator supported
+
+
+```php
+class HomepagePresenter ... {
+
+	/**
+	 * @title homepage.cotact.title
+	 */
+	public function renderContact()
+	{
+	}
+
+}
+```
---
+++ src/Zenify/Components/TitleComponent/UI/Control.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * This file is part of Zenify
+ *
+ * Copyright (c) 2012 Tomas Votruba (http://tomasvotruba.cz)
+ *
+ * For the full copyright and license information, please view
+ * the file license.md that was distributed with this source code.
+ */
+
+namespace Zenify\Components\TitleComponent\UI;
+
+use Nette;
+
+
+class Control extends Nette\Application\UI\Control
+{
+	/** @var string */
+	private $title;
+
+	/** @var Nette\Localization\ITranslator */
+	private $translator;
+
+
+	public function inject(Nette\Localization\ITranslator $translator = NULL)
+	{
+		$this->translator = $translator;
+	}
+
+
+	public function attached($presenter)
+	{
+		parent::attached($presenter);
+
+		$methods[] = $presenter->formatActionMethod($presenter->view);
+		$methods[] = $presenter->formatRenderMethod($presenter->view);
+		foreach ($methods as $method) {
+			if ($presenter->reflection->hasMethod($method)) {
+				$reflectionMethod = $presenter->reflection->getMethod($method);
+
+				if ($title = $reflectionMethod->getAnnotation('title')) {
+					$this->title = $title;
+				}
+			}
+		}
+	}
+
+
+	public function render()
+	{
+		if ($this->translator) {
+			$this->title = $this->translator->translate($this->title);
+		}
+
+		$this->template->title = $this->title;
+		$this->template->setFile(__DIR__ . '/templates/default.latte');
+		$this->template->render();
+	}
+
+
+	public function setTitle($title)
+	{
+		$this->title = $title;
+	}
+
+}
